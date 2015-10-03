da43ec283fa77aad1bcf4b67983fbd0d443e5b59
Guowei Mo <guowei.mo13@gmail.com>
2015-10-03 17:12:44 +0800

    Initial Commit

---
+++ Qashio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Qashio
+//
+//  Created by Guowei Mo on 03/10/2015.
+//  Copyright © 2015 Guowei Mo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
