ad0f8647116af32bff167a1e2e40177b70278c2a
Guowei Mo <guowei.mo@mkodo.com>
2015-11-16 17:44:14 +0000

    [QS-1611] little clearing.

--- Qashio/HomeViewController.swift
+++ Qashio/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        monthLabel.text = self.getCurrentMonth()
+        
     }
     
     func getCurrentMonth() -> String{
