d8054933264b689252fac3a0a35ef644fdef6511
Guowei Mo <guowei.mo@mkodo.com>
2015-12-08 17:34:40 +0000

    [QS-0812] fixed the HTTP request and rebuild the structure.

---
+++ Qashio/APIController.swift
@@ -0,0 +1,65 @@
+//
+//  APIController.swift
+//  Qashio
+//
+//  Created by Guowei Mo on 08/12/2015.
+//  Copyright © 2015 Guowei Mo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController {
+    
+    var delegate: APIControllerProtocol
+    
+    init(delegate: APIControllerProtocol) {
+        self.delegate = delegate
+    }
+    
+    func httpPost(path: String, parameters params: Dictionary<String,AnyObject>?) {
+        if params != nil {
+            Alamofire.request(.POST, path, parameters: params)
+                .responseJSON{ response in
+                    print(response.result)
+                    print(response.request)
+                    let dataResponse = (try! NSJSONSerialization.JSONObjectWithData(response.data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                    
+                    print(dataResponse)
+                    self.delegate.didReceiveAPIResults(dataResponse)
+            }
+        }
+//        let session = NSURLSession.sharedSession()
+//        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+//            print("Task completed")
+//            if(error != nil) {
+//                // If there is an error in the web request, print it to the console
+//                print(error!.localizedDescription)
+//            }
+//            let err: NSError? = nil
+//            print(data!)
+//            let result = (try! NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)) as! NSDictionary
+//            print(result)
+//            if(err != nil) {
+//                // If there is an error parsing JSON, print it to the console
+//                print("JSON Error \(err!.localizedDescription)")
+//            }
+//            self.delegate.didReceiveAPIResults(result)
+//        })
+//        task.resume()
+    }
+    
+    func verifyLogin(params: Dictionary<String,AnyObject>) {
+        let path = "http://198.100.146.69:8080/QashioAPI-1.0/verify"
+        httpPost(path, parameters: params)
+    }
+    
+    func lookupAlbum(collectionId: Int) {
+        
+    }
+    
+}
--- Qashio/LoginViewController.swift
+++ Qashio/LoginViewController.swift
@@ -9,113 +9,103 @@
 import UIKit
 import Alamofire
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, APIControllerProtocol {
     
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var passWord: UITextField!
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var registerBtn: UIButton!
+    var API : APIController?
+    var localData : AnyObject?
+    var loginResults:NSDictionary?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         loginBtn.layer.cornerRadius = 5
         registerBtn.layer.cornerRadius = 5
+        API = APIController(delegate: self)
     }
     
     @IBAction func verifyUserLogin(sender: AnyObject) {
         print("userName: " + userName.text!)
         print("password: " + passWord.text!)
-        let localData = NSUserDefaults.standardUserDefaults()
+        localData = NSUserDefaults.standardUserDefaults()
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
 
-        let storedUserName = localData.valueForKey("USER_NAME") as? String //as? return nil or String
-        let storedPassword = localData.valueForKey("PASSWORD") as? String
+        let storedUserName = localData!.valueForKey("USER_NAME") as? String //as? return nil or String
+        let storedPassword = localData!.valueForKey("PASSWORD") as? String
         
         if storedUserName != nil && !storedPassword!.isEmpty && storedPassword != nil && !storedPassword!.isEmpty {
             print("saved userName: " + storedUserName!)
             print("saved password: " + storedPassword!)
         }
         
-        let result = checkUserInfoWithUserName(userName.text!, userPassword: passWord.text!)
-        
-        print(result!["success"]!)
-        if  result != nil && Bool(result!["success"]! as! NSNumber) == true {
-            //check against the data in db
-                localData.setValue(userName.text!, forKey: "USER_NAME");
-                localData.setValue(passWord.text!, forKey: "PASSWORD");
-            
-                let vc:UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier("mainNav")
-                self.showViewController(vc, sender: vc);
-        } else{
-            let alert:UIAlertController = UIAlertController(title: "ERROR" , message: String(result!["msg"]!), preferredStyle: UIAlertControllerStyle.Alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-            self.presentViewController(alert, animated: true, completion: nil)
-            
-//            let alertController = UIAlertController(title: "Email?", message: "Please input your email:", preferredStyle: .Alert)
-//            
-//            let confirmAction = UIAlertAction(title: "Confirm", style: .Default) { (_) in
-//                if let field:UITextField = alertController.textFields![0] {
-//                    // store your data
-//                    NSUserDefaults.standardUserDefaults().setObject(field.text, forKey: "userEmail")
-//                    NSUserDefaults.standardUserDefaults().synchronize()
-//                }
-//            }
-//            
-//            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
-//            
-//            //add the input field
-//            alertController.addTextFieldWithConfigurationHandler { (textField) in
-//                textField.placeholder = "Email"
-//            }
-//            
-//            alertController.addAction(confirmAction)
-//            alertController.addAction(cancelAction)
-//            
-//            self.presentViewController(alertController, animated: true, completion: nil)
-        }
-    }
-    
-    func checkUserInfoWithUserName(username: String, userPassword password: String) -> Dictionary<String, AnyObject>?{
-        
         let params = [
-            "username"  : username,
-            "password"  : password,
+            "username"  : userName.text!,
+            "password"  : passWord.text!,
             "type"      : "check"
         ]
-        Alamofire.request(.POST, "http://198.100.146.69:8080/QashioAPI-1.0/verify", parameters: params)
-            .responseJSON{ response in
-                print(response.result)
-                print(response.request)
-                let dataResponse = (try! NSJSONSerialization.JSONObjectWithData(response.data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
-
-                print(dataResponse)
-        }
-
-        var result = Dictionary<String, AnyObject>()
-
-        let temV = arc4random_uniform(100)%3
-        print(temV)
-        if  temV == 0 {
-            result = [
-                "success"   : false,
-                "msg"       : "user does not exist"
-            ]
-        } else if  temV == 1{
-            result = [
-                "success"   : false,
-                "msg"       : "the password is not correct for the user"
-            ]
-        }else{
-            result = [
-                "success"   : true,
-                "msg"       : ""
-            ]
+        
+        API?.verifyLogin(params)
+    }
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        //        dispatch_async(dispatch_get_main_queue(), {
+        self.loginResults = results
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        //        })
+        renderLoginResult()
+    }
+    
+    func renderLoginResult(){
+        var errorMsg = ""
+        if self.loginResults != nil {
+            if self.loginResults!["success"]! as! NSNumber == 1 {
+                //check against the data in db
+                localData!.setValue(userName.text!, forKey: "USER_NAME");
+                localData!.setValue(passWord.text!, forKey: "PASSWORD");
+                
+                let vc:UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier("mainNav")
+                self.showViewController(vc, sender: vc);
+            } else{
+                errorMsg = String(self.loginResults!["msg"]!)
+                if errorMsg.isEmpty {
+                    errorMsg = "Unable to Log in."
+                }
+                
+                //            let alertController = UIAlertController(title: "Email?", message: "Please input your email:", preferredStyle: .Alert)
+                //
+                //            let confirmAction = UIAlertAction(title: "Confirm", style: .Default) { (_) in
+                //                if let field:UITextField = alertController.textFields![0] {
+                //                    // store your data
+                //                    NSUserDefaults.standardUserDefaults().setObject(field.text, forKey: "userEmail")
+                //                    NSUserDefaults.standardUserDefaults().synchronize()
+                //                }
+                //            }
+                //
+                //            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
+                //
+                //            //add the input field
+                //            alertController.addTextFieldWithConfigurationHandler { (textField) in
+                //                textField.placeholder = "Email"
+                //            }
+                //
+                //            alertController.addAction(confirmAction)
+                //            alertController.addAction(cancelAction)
+                //            
+                //            self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        } else {
+            errorMsg = "Network Error. Please try again."
         }
         
-        if username.isEmpty && password.isEmpty{
-        
+        if !errorMsg.isEmpty {
+            let alert:UIAlertController = UIAlertController(title: "ERROR" , message: errorMsg, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
         }
-        
-        return result
+ 
     }
  
     
