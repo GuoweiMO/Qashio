1e98bbe3bc4c57e9199ccfacc375c644a264c2e9
Guowei Mo <guowei.mo@mkodo.com>
2015-12-10 17:57:09 +0000

    [QS-1012] fixed the test coverage

--- QashioTests/CategoryTests.swift
+++ QashioTests/CategoryTests.swift
@@ -10,7 +10,6 @@ import XCTest
 @testable import Qashio
 
 class CategoryTests: XCTestCase {
-    let categoryObj = Category()
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -27,16 +26,19 @@ class CategoryTests: XCTestCase {
     }
     
     func testCategoryData(){
+        let categoryObj = Qashio.Category()
         XCTAssertNotNil(categoryObj.categoryData, "category data is not nil")
         XCTAssertEqual(categoryObj.categoryData.count, 10, "category data has 10 items")
     }
     
     func testCateResData(){
+        let categoryObj = Qashio.Category()
         XCTAssertNotNil(categoryObj.catResource, "category resource is not nil")
         XCTAssertEqual(categoryObj.catResource.count, 10,"category resource has 10 items")
     }
     
     func testAddSubCategory(){
+        let categoryObj = Qashio.Category()
         XCTAssertEqual(categoryObj.selectedCategories.count, 0, "The initial selected category list is empty")
         categoryObj.addSelectedSubCategory("IT", subCategoryToAdd: "Cloud")
         XCTAssertEqual(categoryObj.selectedCategories.count, 1, "The count of selected category is 1")
@@ -44,6 +46,7 @@ class CategoryTests: XCTestCase {
     }
     
     func testRemoveSubCategory(){
+        let categoryObj = Qashio.Category()
         XCTAssertEqual(categoryObj.selectedCategories.count, 0, "The initial selected category list is empty")
         categoryObj.addSelectedSubCategory("IT", subCategoryToAdd: "Cloud")
         categoryObj.removeunSelectedSubCategory("IT", subCategoryToRemove: "Cloud")
@@ -51,14 +54,8 @@ class CategoryTests: XCTestCase {
     }
     
     func testSameKeysForCategoryAndResource(){
+        let categoryObj = Qashio.Category()
         XCTAssertEqual(Array(categoryObj.categoryData.keys), Array(categoryObj.catResource.keys))
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
--- QashioTests/LocationsTests.swift
+++ QashioTests/LocationsTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Qashio
 
 class LocationsTests: XCTestCase {
     override func setUp() {
@@ -23,12 +24,4 @@ class LocationsTests: XCTestCase {
         XCTAssertNotNil(Locations.LocConf.locations)
         XCTAssertEqual(Locations.LocConf.locations.count, 3)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
